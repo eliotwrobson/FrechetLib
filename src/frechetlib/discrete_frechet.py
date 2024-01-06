82e4bf73926c65ec99ffb85557a4d5c1d8068933
Eliot W. Robson <eliot.robson24@gmail.com>
2024-01-06 15:42:45 -0500

    Faster function

--- src/frechetlib/discrete_frechet.py
+++ src/frechetlib/discrete_frechet.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+from typing import Callable
+
 import numpy as np
+from numba import jit
 
 
 def _c(ca, i, j, p, q):
@@ -38,10 +41,35 @@ def discrete_frechet_distance(P: np.ndarray, Q: np.ndarray) -> np.float64:
     if len_p == 0 or len_q == 0:
         raise ValueError("Input curves are empty.")
 
-    if len_p != len_q or len(p[0]) != len(q[0]):
-        raise ValueError("Input curves do not have the same dimensions.")
+    # if len_p != len_q or len(p[0]) != len(q[0]):
+    #    raise ValueError("Input curves do not have the same dimensions.")
 
     ca = np.ones((len_p, len_q), dtype=np.float64) * -1
 
     dist = _c(ca, len_p - 1, len_q - 1, p, q)
     return dist
+
+
+@jit(nopython=True)
+def linear_frechet(p: np.ndarray, q: np.ndarray) -> float:
+    """
+    From:
+    https://github.com/joaofig/discrete-frechet/blob/master/recursive-vs-linear.ipynb
+    """
+    n_p = p.shape[0]
+    n_q = q.shape[0]
+    ca = np.zeros((n_p, n_q), dtype=np.float64)
+
+    for i in range(n_p):
+        for j in range(n_q):
+            d = np.linalg.norm(p[i] - q[j])
+
+            if i > 0 and j > 0:
+                ca[i, j] = max(min(ca[i - 1, j], ca[i - 1, j - 1], ca[i, j - 1]), d)
+            elif i > 0 and j == 0:
+                ca[i, j] = max(ca[i - 1, 0], d)
+            elif i == 0 and j > 0:
+                ca[i, j] = max(ca[0, j - 1], d)
+            else:
+                ca[i, j] = d
+    return ca[n_p - 1, n_q - 1]
--- tests/test_discrete_frechet.py
+++ tests/test_discrete_frechet.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from frechetlib.discrete_frechet import discrete_frechet_distance
+from frechetlib.discrete_frechet import linear_frechet
 
 TEST_CASES = [
     {"P": [[1, 1], [2, 1], [2, 2]], "Q": [[2, 2], [0, 1], [2, 4]], "expected": 2.0},
@@ -20,27 +20,27 @@ TEST_CASES = [
         "Q": np.array((np.linspace(0.0, 1.0, 100), np.ones(100))).T,
         "expected": 1.0,
     },
+    {"P": [[1, 1], [2, 1]], "Q": [[2, 2], [0, 1], [2, 4]], "expected": 3.0},
+    {
+        "P": [[0.0, 0.0], [1.0, 0.0], [2.0, 0.0], [3.0, 0.0], [4.0, 0.0]],
+        "Q": [[0.0, 1.0], [1.0, 1.1], [2.0, 1.2], [3.0, 1.1], [4.0, 1.0]],
+        "expected": 1.2,
+    },
 ]
 
 
 def test_discrete_frechet() -> None:
     for test_case in TEST_CASES:
-        P = test_case["P"]
-        Q = test_case["Q"]
+        P = np.array(test_case["P"], np.float64)
+        Q = np.array(test_case["Q"], np.float64)
         eo = test_case["expected"]
 
-        assert discrete_frechet_distance(P, Q) == eo
+        assert linear_frechet(P, Q) == eo
 
 
-def test_errors():
-    P = [[1, 1], [2, 1]]
-    Q = [[2, 2], [0, 1], [2, 4]]
-
-    with pytest.raises(ValueError):
-        assert discrete_frechet_distance(P, Q) == 2.0
-
+def test_errors() -> None:
     P = []
     Q = [[2, 2], [0, 1], [2, 4]]
 
     with pytest.raises(ValueError):
-        assert discrete_frechet_distance(P, Q) == 2.0
+        assert linear_frechet(P, Q) == 2.0
