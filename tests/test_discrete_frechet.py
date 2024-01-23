e94fbb0ddc1494843c57e4b0c3739b89ce10d167
Eliot Robson <eliot.robson24@gmail.com>
2024-01-23 13:55:16 -0600

    Split up test cases

--- tests/test_discrete_frechet.py
+++ tests/test_discrete_frechet.py
@@ -1,19 +1,45 @@
 import typing as t
 
 import numpy as np
+import numpy.typing as npt
 import pytest
 
 import frechetlib.discrete_frechet as df
 
-DISCRETE_FRECHET_FUNCS = (df.linear_frechet, df.linear_frechet_2)
 FrechetDistFuncT = t.Callable[[np.ndarray, np.ndarray], np.float64]
+CurveGeneratorFunctionT = t.Callable[[int, float], tuple[np.ndarray, np.ndarray]]
 
 
-def test_frechet_equal() -> None:
+DISCRETE_FRECHET_FUNCS = (df.linear_frechet, df.linear_frechet_2)
+
+# Generator helper functions
+
+
+def generate_curves_random(
+    num_pts: int, scaling_factor: float
+) -> tuple[np.ndarray, np.ndarray]:
+    P = np.random.rand(num_pts, 2) * scaling_factor
+    Q = np.random.rand(num_pts, 2) * scaling_factor
+    return (P, Q)
+
+
+def generate_curves_close(
+    num_pts: int, scaling_factor: float
+) -> tuple[np.ndarray, np.ndarray]:
+    P = np.random.uniform(low=1.0, high=scaling_factor, size=(num_pts, 2))
+    Q = P + np.random.uniform(low=0.0, high=0.5, size=(num_pts, 2))
+    return (P, Q)
+
+
+# Start of actual test functions
+
+
+@pytest.mark.parametrize(
+    "generate_curves", [generate_curves_random, generate_curves_close]
+)
+def test_frechet_equal(generate_curves: CurveGeneratorFunctionT) -> None:
     n = 1000
-    P = np.random.rand(n, 2)
-    Q = np.random.rand(n, 2)
-
+    P, Q = generate_curves(n, 100.0)
     assert np.isclose(df.linear_frechet(P, Q), df.linear_frechet_2(P, Q))
 
 
@@ -21,54 +47,18 @@ def test_frechet_equal() -> None:
     "frechet_dist_func",
     DISCRETE_FRECHET_FUNCS,
 )
-def test_frechet_benchmark_random(
-    benchmark: t.Any, frechet_dist_func: FrechetDistFuncT
-) -> None:
-    n = 1000
-    P = np.random.rand(n, 2) * 100
-    Q = np.random.rand(n, 2) * 100
-
-    benchmark(frechet_dist_func, P, Q)
-
-
 @pytest.mark.parametrize(
-    "frechet_dist_func",
-    DISCRETE_FRECHET_FUNCS,
+    "generate_curves", [generate_curves_random, generate_curves_close]
 )
-def test_frechet_benchmark_close(
-    benchmark: t.Any, frechet_dist_func: FrechetDistFuncT
+@pytest.mark.parametrize("n", [100, 1000])
+def test_frechet_benchmark(
+    benchmark: t.Any,
+    frechet_dist_func: FrechetDistFuncT,
+    generate_curves: CurveGeneratorFunctionT,
+    n: int,
 ) -> None:
-    n = 10_000
-    P = np.random.uniform(low=1.0, high=100, size=(n, 2))
-    Q = P + np.random.uniform(low=0.0, high=0.5, size=(n, 2))
-
+    P, Q = generate_curves(n, 100.0)
     benchmark(frechet_dist_func, P, Q)
-
-
-TEST_CASES = [
-    {"P": [[1, 1], [2, 1], [2, 2]], "Q": [[2, 2], [0, 1], [2, 4]], "expected": 2.0},
-    {
-        "P": np.array((np.linspace(0.0, 1.0, 100), np.ones(100))).T,
-        "Q": np.array((np.linspace(0.0, 1.0, 100), np.ones(100))).T,
-        "expected": 0,
-    },
-    {
-        "P": [[-1, 0], [0, 1], [1, 0], [0, -1]],
-        "Q": [[-2, 0], [0, 2], [2, 0], [0, -2]],
-        "expected": 1.0,
-    },
-    {
-        "P": np.array((np.linspace(0.0, 1.0, 100), np.ones(100) * 2)).T,
-        "Q": np.array((np.linspace(0.0, 1.0, 100), np.ones(100))).T,
-        "expected": 1.0,
-    },
-    {"P": [[1, 1], [2, 1]], "Q": [[2, 2], [0, 1], [2, 4]], "expected": 3.0},
-    {
-        "P": [[0.0, 0.0], [1.0, 0.0], [2.0, 0.0], [3.0, 0.0], [4.0, 0.0]],
-        "Q": [[0.0, 1.0], [1.0, 1.1], [2.0, 1.2], [3.0, 1.1], [4.0, 1.0]],
-        "expected": 1.2,
-    },
-]
 
 
 @pytest.mark.parametrize(
@@ -99,9 +89,11 @@ TEST_CASES = [
     ],
 )
 def test_discrete_frechet(
-    frechet_dist_func: FrechetDistFuncT, P: np.ndarray, Q: np.ndarray, expected: float
+    frechet_dist_func: FrechetDistFuncT,
+    P: npt.ArrayLike,
+    Q: npt.ArrayLike,
+    expected: float,
 ) -> None:
-    # for test_case in TEST_CASES:
     P = np.array(P, np.float64)
     Q = np.array(Q, np.float64)
 
