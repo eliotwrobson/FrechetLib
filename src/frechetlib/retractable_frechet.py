d8cfe2473ac7543933f747dc7b5e11d4f1db9d22
Eliot Robson <eliot.robson24@gmail.com>
2024-01-25 15:45:52 -0600

    Deleted unused code

--- src/frechetlib/retractable_frechet.py
+++ src/frechetlib/retractable_frechet.py
@@ -11,36 +11,6 @@ from numba.experimental import jitclass
 from typing_extensions import Self
 
 
-class EventType(IntEnum):
-    POINT_VERTEX = 1
-    POINT_ON_EDGE = 2
-
-
-@jitclass(
-    [("p", nb.float64[:]), ("other_point", nb.float64[:]), ("event_type", nb.int32)]
-)
-class EventPoint:
-    p: np.ndarray
-    i: int
-    event_type: EventType
-    t: float
-    other_point: np.ndarray
-
-    def __init__(
-        self,
-        p_: np.ndarray,
-        i_: int,
-        event_type_: EventType,
-        t_: float,
-        other_point_: np.ndarray,
-    ) -> None:
-        self.p = p_
-        self.i = i_
-        self.event_type = event_type_
-        self.t = t_
-        self.other_point = other_point_
-
-
 @jitclass
 class EID:
     i: int
