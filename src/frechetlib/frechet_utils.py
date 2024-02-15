e4002417feefee021622862dc4a7d69f54217319
Eliot Robson <eliot.robson24@gmail.com>
2024-02-15 00:32:25 -0600

    Update frechet_utils.py

--- src/frechetlib/frechet_utils.py
+++ src/frechetlib/frechet_utils.py
@@ -206,3 +206,25 @@ def morphing_combine(
             idx_1 = min(idx_1 + 1, len_1)
 
     return res
+
+
+def extract_vertex_radii(
+    P: np.ndarray, Q: np.ndarray, morphing: list[EID]
+) -> tuple[np.ndarray, np.ndarray]:
+    """
+    For each vertex in either polygon, take the maximum leash length
+    on the given vertices.
+    """
+
+    P_leash_lens = np.zeros(P.shape[0], dtype=np.float64)
+    Q_leash_lens = np.zeros(Q.shape[0], dtype=np.float64)
+
+    for k in range(len(morphing)):
+        event = morphing[k]
+        if event.i_is_vert:
+            P_leash_lens[event.i] = np.max(P_leash_lens[event.i], event.dist)
+
+        if event.j_is_vert:
+            Q_leash_lens[event.j] = np.max(Q_leash_lens[event.j], event.dist)
+
+    return P_leash_lens, Q_leash_lens
