d462c05f7384065389030be0761d425e6c890e4a
Eliot Robson <eliot.robson24@gmail.com>
2024-02-16 09:02:49 -0600

    Added stuff to debug

--- tests/test_continuous_frechet.py
+++ tests/test_continuous_frechet.py
@@ -6,16 +6,6 @@ import frechetlib.frechet_utils as fu
 import frechetlib.retractable_frechet as rf
 
 
-def test_frechet_mono_via_refinement() -> None:
-    P = np.array([[0.0, 0.0], [1.0, 1.0]])
-    Q = np.array([[0.0, 0.0], [0.5, 0.5], [0.3, 0.3], [0.7, 0.7], [1.0, 1.0]])
-
-    P, Q, monotone_morphing, dist, f_exact = cf.frechet_mono_via_refinement(P, Q, 1.01)
-    print(len(monotone_morphing))
-    # TODO uncomment and finish debugging this
-    # assert False
-
-
 def test_get_monotone_morphing_width() -> None:
     # TODO check with Sariel about this test case
     P = np.array([[0.0, 0.0], [1.0, 1.0]])
@@ -31,8 +21,18 @@ def test_get_monotone_morphing_width() -> None:
             fu.EID(1, True, 4, True, P, Q),
         ]
     )
-    _, res = cf.get_monotone_morphing_width(morphing)
+    _, res = cf.get_monotone_morphing_width(morphing, P, Q)
     assert len(res) == 3
+
+
+def test_frechet_mono_via_refinement() -> None:
+    P = np.array([[0.0, 0.0], [1.0, 1.0]])
+    Q = np.array([[0.0, 0.0], [0.5, 0.5], [0.3, 0.3], [0.7, 0.7], [1.0, 1.0]])
+
+    P, Q, monotone_morphing, dist, f_exact = cf.frechet_mono_via_refinement(P, Q, 1.01)
+    print(len(monotone_morphing))
+    # TODO uncomment and finish debugging this
+    # assert False
 
 
 def test_frechet_add_points() -> None:
