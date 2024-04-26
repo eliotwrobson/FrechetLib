5f22082228b48a985adc7613e8d364bb2ee065c5
Eliot Robson <eliot.robson24@gmail.com>
2024-04-26 12:59:36 -0500

    First version of the thing

--- src/frechetlib/sweep_frechet.py
+++ src/frechetlib/sweep_frechet.py
@@ -1,3 +1,5 @@
+import heapq as hq
+
 import numpy as np
 from numba import njit  # type: ignore[attr-defined]
 
@@ -29,3 +31,57 @@ def sweep_frechet_compute_refine_mono(P: np.ndarray, Q: np.ndarray) -> fu.Morphi
         P, Q = fu.add_points_to_make_monotone(morphing)
 
     return morphing
+
+
+@njit
+def get_edge_value(i: int, j: int, P: np.ndarray, Q: np.ndarray) -> float:
+    n_p = P.shape[0]
+    assert 0 <= i <= n_p
+    weight = np.inf
+
+    if j > 1:
+        dist = fu.line_line_distance(P[i], P[i + 1], Q[j - 1], Q[j])
+        weight = max(weight, dist)
+    if j < Q.shape[0]:
+        dist = fu.line_line_distance(P[i], P[i + 1], Q[j], Q[j + 1])
+        weight = max(weight, dist)
+
+    length = float(np.linalg.norm(P[i] - P[i + 1]))
+    return weight * length
+
+
+@njit
+def sweep_frechet_compute_lower_bound(P: np.ndarray, Q: np.ndarray) -> float:
+    work_queue = [(0.0, 0, 0)]
+    prev_weight = {(0, 0): 0.0}
+
+    n_p = P.shape[0]
+    n_q = Q.shape[0]
+
+    res = 0.0
+
+    while work_queue:
+        weight, i, j = hq.heappop(work_queue)
+
+        res = max(res, weight)
+
+        if i == n_p and j == n_q:
+            break
+
+        if i < n_p:
+            new_weight = weight + get_edge_value(i, j, P, Q)
+
+            new_idx = (i + 1, j)
+            if prev_weight.get(new_idx, np.inf) > new_weight:
+                prev_weight[new_idx] = new_weight
+                hq.heappush(work_queue, (new_weight, i + 1, j))
+
+        if j < n_q:
+            new_weight = weight + get_edge_value(j, i, Q, P)
+
+            new_idx = (i, j + 1)
+            if prev_weight.get(new_idx, np.inf) > new_weight:
+                prev_weight[new_idx] = new_weight
+                hq.heappush(work_queue, (new_weight, i, j + 1))
+
+    return res
