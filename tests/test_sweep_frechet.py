6c1407ef1271a9bca30b214f0f78a6e9df34166e
Eliot Robson <eliot.robson24@gmail.com>
2024-05-02 22:58:27 -0500

    Added test file for sweep (incomplete rn)

---
+++ tests/test_sweep_frechet.py
@@ -0,0 +1,12 @@
+import frechetlib.data as fld
+import frechetlib.sweep_frechet as sf
+
+
+def test_sweep_frechet_refinement(frechet_downloader: fld.FrechetDownloader) -> None:
+    P_curve = frechet_downloader.get_curve("03/poly_a.txt")
+    Q_curve = frechet_downloader.get_curve("03/poly_b.txt")
+    sweep_morphing = sf.sweep_frechet_compute_refine_mono(P_curve, Q_curve)
+
+    print(len(sweep_morphing.morphing_list))
+    sf.sweep_frechet_compute_lower_bound(P_curve, Q_curve)
+    assert False
