cf7e8ead4acaa2bbf3e7d1c719c89c71dabd8e5e
Eliot W. Robson <eliot.robson24@gmail.com>
2024-05-12 21:48:38 -0400

    Most of test bench written

--- paper_bench/run_experiments.py
+++ paper_bench/run_experiments.py
@@ -1,11 +1,11 @@
 import time
 import zipfile
-from itertools import product
 
 import numpy as np
 import pandas as pd
 import pooch
-from frechetlib.continuous_frechet import frechet_c_approx
+from frechetlib.continuous_frechet import frechet_c_approx, frechet_c_compute
+from frechetlib.retractable_frechet import retractable_ve_frechet
 
 ZIP_HASH = "ad5a1d8585769e36bda87a018573831d145af75e8303a063f3de64ed35ed1da9"
 ZIP_URL = "http://sarielhp.org/misc/blog/24/05/10/data_e.zip"
@@ -29,11 +29,11 @@ def main() -> None:
     curve_numbers = list(range(1, 9))
     # TODO see what's going on with the lower factor (1.001). This isn't
     # getting stuck in Sariel's code, so something weird may be happening.
-    factors = [1.001, 1.1, 4.0]
+    factors = [1.009, 1.1, 4.0]
 
     # TODO add exact computation and raw VE computation workloads.
     results = []
-    for factor, curve_num in product(factors, curve_numbers):
+    for curve_num in curve_numbers:
         try:
             p_curve = np.genfromtxt(
                 data_archive.open(f"data/test/00{curve_num}_p.plt"), delimiter=","
@@ -45,22 +45,56 @@ def main() -> None:
             print(f"Could not read curve file {curve_num}, skipping.")
             continue
 
-        print(f"Starting workload {curve_num} with approx factor {factor}")
+        # Run with approx factors first
+        for factor in factors:
+            print(f"Starting workload {curve_num} with approx factor {factor}")
+            start = time.perf_counter()
+            ratio, morphing = frechet_c_approx(p_curve, q_curve, factor)
+            time_taken = time.perf_counter() - start
+            print(f"Workload complete in {time_taken:4f} seconds.")
+
+            res_dict = {
+                "Curve Number": curve_num,
+                "Algorithm": f"Approx with factor {factor}",
+                "Time Taken": time_taken,
+                "Distance": morphing.dist,
+            }
+
+            results.append(res_dict)
+
+        # Then, run exact
+        print(f"Starting workload {curve_num} exact")
         start = time.perf_counter()
-        ratio, morphing = frechet_c_approx(p_curve, q_curve, factor)
+        morphing = frechet_c_compute(p_curve, q_curve)
         time_taken = time.perf_counter() - start
         print(f"Workload complete in {time_taken:4f} seconds.")
 
         res_dict = {
             "Curve Number": curve_num,
-            "Approx Factor": factor,
+            "Algorithm": "Exact",
             "Time Taken": time_taken,
+            "Distance": morphing.dist,
         }
-        exit()
+
+        # Finally, run the really slow ve-r
+        print(f"Starting workload {curve_num} ve-r")
+        start = time.perf_counter()
+        morphing = retractable_ve_frechet(p_curve, q_curve, None, None, False)
+        time_taken = time.perf_counter() - start
+        print(f"Workload complete in {time_taken:4f} seconds.")
+
+        res_dict = {
+            "Curve Number": curve_num,
+            "Algorithm": "VER",
+            "Time Taken": time_taken,
+            "Distance": morphing.dist,
+        }
+
         results.append(res_dict)
 
-        df = pd.DataFrame(results)
-        df.to_csv("benchmark_results.csv")
+    # Export test data to CSV file.
+    df = pd.DataFrame(results)
+    df.to_csv("benchmark_results.csv")
 
 
 if __name__ == "__main__":
