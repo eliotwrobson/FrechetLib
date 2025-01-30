937fd68b1bab96cde7ff70b7072d144451393483
Eliot Robson <eliot.robson24@gmail.com>
2025-01-30 16:05:06 -0600

    Added another test

--- tests/test_frechet_utils.py
+++ tests/test_frechet_utils.py
@@ -271,6 +271,57 @@ def test_add_points() -> None:
     assert np.allclose(new_Q, Q)
 
 
+def test_add_points_to_make_monotone() -> None:
+    P = np.array([[0.0, 0.0], [1.0, 1.0]])
+    Q = np.array([[0.0, 0.0], [0.5, 0.5], [0.3, 0.3], [0.7, 0.7], [1.0, 1.0]])
+
+    morphing = rf.retractable_ve_frechet(P, Q, None, None, False)
+    new_curves = fu.add_points_to_make_monotone(morphing)
+    new_P, new_Q = new_curves.get_P(), new_curves.get_Q()
+
+    new_P_expected = np.array(
+        [
+            [0.0, 0.0],
+            [0.15, 0.15],
+            [0.3, 0.3],
+            [0.4, 0.4],
+            [0.5, 0.5],
+            [0.6, 0.6],
+            [0.7, 0.7],
+            [0.85, 0.85],
+            [1.0, 1.0],
+        ]
+    )
+
+    assert np.allclose(new_P, new_P_expected)
+    assert np.allclose(new_Q, Q)
+
+    # Extra zeroes because of a quirk with the ve-frechet algo
+    new_P_expected = np.array(
+        [
+            [0.0, 0.0],
+            [0.0, 0.0],
+            [0.0, 0.0],
+            [0.15, 0.15],
+            [0.3, 0.3],
+            [0.4, 0.4],
+            [0.5, 0.5],
+            [0.6, 0.6],
+            [0.7, 0.7],
+            [0.85, 0.85],
+            [1.0, 1.0],
+        ]
+    )
+
+    # Assert the flipped side because of code structure
+    morphing = rf.retractable_ve_frechet(Q, P, None, None, False)
+    new_curves = fu.add_points_to_make_monotone(morphing)
+    new_Q, new_P = new_curves.get_P(), new_curves.get_Q()
+
+    assert np.allclose(new_P, new_P_expected)
+    assert np.allclose(new_Q, Q)
+
+
 '''
 
 
@@ -481,53 +532,4 @@ def test_morphing_make_monotone() -> None:
     assert len(other_morphing) == len(non_monotone_morphing)
     check_morphing_witness(other_morphing)
     assert other_morphing.get_dist() >= non_monotone_morphing.get_dist()
-
-
-def test_add_points_to_make_monotone() -> None:
-    P = np.array([[0.0, 0.0], [1.0, 1.0]])
-    Q = np.array([[0.0, 0.0], [0.5, 0.5], [0.3, 0.3], [0.7, 0.7], [1.0, 1.0]])
-
-    morphing = rf.retractable_ve_frechet(P, Q, None, None, False)
-    new_P, new_Q = fu.add_points_to_make_monotone(morphing)
-
-    new_P_expected = np.array(
-        [
-            [0.0, 0.0],
-            [0.15, 0.15],
-            [0.3, 0.3],
-            [0.4, 0.4],
-            [0.5, 0.5],
-            [0.6, 0.6],
-            [0.7, 0.7],
-            [0.85, 0.85],
-            [1.0, 1.0],
-        ]
-    )
-
-    assert np.allclose(new_P, new_P_expected)
-    assert np.allclose(new_Q, Q)
-
-    # Extra zeroes because of a quirk with the ve-frechet algo
-    new_P_expected = np.array(
-        [
-            [0.0, 0.0],
-            [0.0, 0.0],
-            [0.0, 0.0],
-            [0.15, 0.15],
-            [0.3, 0.3],
-            [0.4, 0.4],
-            [0.5, 0.5],
-            [0.6, 0.6],
-            [0.7, 0.7],
-            [0.85, 0.85],
-            [1.0, 1.0],
-        ]
-    )
-
-    # Assert the flipped side because of code structure
-    morphing = rf.retractable_ve_frechet(Q, P, None, None, False)
-    new_Q, new_P = fu.add_points_to_make_monotone(morphing)
-
-    assert np.allclose(new_P, new_P_expected)
-    assert np.allclose(new_Q, Q)
 '''
